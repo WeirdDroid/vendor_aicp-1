389199fce469a79a46debd223dcb51a5dad9d367
mosimchah <mosimchah@gmail.com>
2019-09-03 13:27:25 -0400

    vendor_aicp: misc updates for 10.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/AICP/vendor_jenkins/p9.0/aicp-build-targets   | sed -e 's/#.*$//' | grep p9.0 | awk {'print $2'})
+for combo in $(curl -s https://raw.githubusercontent.com/AICP/vendor_jenkins/q10.0/aicp-build-targets   | sed -e 's/#.*$//' | grep q10.0 | awk {'print $2'})
 do
     add_lunch_combo $combo
 done
